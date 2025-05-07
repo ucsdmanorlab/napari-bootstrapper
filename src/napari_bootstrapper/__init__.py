5330a5514dd6e9edf11c1ae7e05b5baad72c2c90
yajivunev <vvenu@utexas.edu>
2025-05-06 19:20:38 -0500

    Remove reader and writer templates

--- src/napari_bootstrapper/__init__.py
+++ src/napari_bootstrapper/__init__.py
@@ -1,6 +1,5 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
-from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
 from ._widget import (
     ExampleQWidget,
@@ -8,12 +7,8 @@ from ._widget import (
     threshold_autogenerate_widget,
     threshold_magic_widget,
 )
-from ._writer import write_multiple, write_single_image
 
 __all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
     "make_sample_data",
     "ExampleQWidget",
     "ImageThreshold",
