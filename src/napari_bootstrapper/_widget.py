8603d09a32512f82f049d9cef6c62b5d74aa1b89
yajivunev <vvenu@utexas.edu>
2025-05-20 11:58:33 -0500

    temp remove waterz + refactor seg modules

--- src/napari_bootstrapper/_widget.py
+++ src/napari_bootstrapper/_widget.py
@@ -30,25 +30,26 @@ from qtpy.QtWidgets import (
 )
 from tqdm import tqdm
 
-from .cc import cc_from_affinities
+
 from .datasets.napari_2d_dataset import Napari2DDataset
 from .datasets.napari_3d_dataset import Napari3DDataset
 from .gp.napari_image_source import NapariImageSource
 from .gp.np_source import NpArraySource
 from .gp.torch_predict import torchPredict
 from .models import get_2d_model, get_3d_model, get_loss
-from .mws import mwatershed_from_affinities
-from .ws import watershed_from_affinities
+from .post.cc import cc_from_affinities
+from .post.mws import mwatershed_from_affinities
+# from .post.ws import watershed_from_affinities
 
 DEFAULT_SEG_PARAMS = {
-    "watershed": {
-        "sigma": None,
-        "noise_eps": None,
-        "bias": None,
-        "threshold": 0.5,
-        "min_seed_distance": 10,
-        "fragments_in_xy": True,
-    },
+    # "watershed": {
+    #     "sigma": None,
+    #     "noise_eps": None,
+    #     "bias": None,
+    #     "threshold": 0.5,
+    #     "min_seed_distance": 10,
+    #     "fragments_in_xy": True,
+    # },
     "mutex watershed": {
         "sigma": None,
         "noise_eps": 0.001,
@@ -131,9 +132,9 @@ def segment_affs(affs, method="mutex watershed", params=DEFAULT_SEG_PARAMS):
     print(
         f"Segmenting affs of shape {affs.shape} with method {method} and params {params[method]}"
     )
-    if method == "watershed":
-        return watershed_from_affinities(affs[:3], **params[method])
-    elif method == "mutex watershed":
+    # if method == "watershed":
+    #     return watershed_from_affinities(affs[:3], **params[method])
+    if method == "mutex watershed":
         return mwatershed_from_affinities(affs, **params[method])
     elif method == "connected components":
         return cc_from_affinities(affs, **params[method])
@@ -518,7 +519,10 @@ class Widget(QMainWindow):
         self.seg_method_label = QLabel("Segmentation method:")
         self.seg_method_selector = QComboBox()
         self.seg_method_selector.addItems(
-            ["watershed", "mutex watershed", "connected components"]
+            [
+                #"watershed", 
+                "mutex watershed", 
+                "connected components"]
         )
         self.seg_method_selector.setCurrentText("mutex watershed")
 
